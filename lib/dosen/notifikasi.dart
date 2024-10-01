e33061418526164462cf00b19365a41ba79475ad
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-10-01 10:03:31 +0700

    halaman profil

--- lib/dosen/notifikasi.dart
+++ lib/dosen/notifikasi.dart
@@ -4,7 +4,6 @@ import '../header.dart'; // Import file header.dart
 class NotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
--- lib/dosen/profil.dart
+++ lib/dosen/profil.dart
@@ -3,8 +3,74 @@ import 'package:flutter/material.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 50.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Color(0xFF002366),
+                    child: Icon(
+                      Icons.person,
+                      size: 40,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Text(
+                    'Zulfa Ulinnuha',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '222222222',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    'Dosen Jurusan Teknologi Informasi',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 40.0),
+            ListTile(
+              leading: Icon(Icons.edit, color: Colors.black),
+              title: Text(
+                'Edit Profil',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              onTap: () {
+                // Aksi ketika "Edit Profil" ditekan
+              },
+            ),
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.logout, color: Colors.black),
+              title: Text(
+                'Keluar',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              onTap: () {
+                // Aksi ketika "Keluar" ditekan
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
