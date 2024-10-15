ab593c34109cd1c4dfd0bb06138008844e1b0afa
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-10-15 13:03:51 +0700

    progress dosen

--- lib/dosen/descRiwayat.dart
+++ lib/dosen/descRiwayat.dart
@@ -1,101 +1,141 @@
 import 'package:flutter/material.dart';
+import '../header.dart'; // Import file header.dart
 
 class DescRiwayat extends StatelessWidget {
-  final String title;
-  final String subtitle;
-  final String description;
-
-  DescRiwayat({
-    required this.title,
-    required this.subtitle,
-    required this.description,
-  });
-
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Title and Subtitle
-            Text(
-              title,
-              style: TextStyle(
-                fontSize: 26, // Larger font size for title
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: height * 0.04),
+              Stack(
+                children: [
+                  Header(
+                      userName: 'Zulfa Ulinnuha'), // Reuse the Header component
+                  Positioned(
+                    left: 0,
+                    top: 16,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        width: 40, // Adjust the size as per your design
+                        height: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        child: Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.black,
+                          size: 20, // Adjust icon size to fit inside the circle
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: 4),
-            Text(
-              subtitle,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[700],
+              SizedBox(height: height * 0.01),
+              Text(
+                'AWS Certified Solutions Architect',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-
-            // Description Box
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey[300]!), // Light grey border
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Text(
-                description,
+              Text(
+                'Cloud Computing',
                 style: TextStyle(
                   fontSize: 16,
-                  color: Colors.black87,
+                  color: Colors.grey,
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-
-            // Button for scheduling exam
-            Spacer(),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                width: width * 0.9, // Width of the button closer to full width
-                height: 50,
+              SizedBox(height: height * 0.02),
+              Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Online proctoring:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: height * 0.01),
+                    Text(
+                      'Online proctoring is a testing environment that allows you to take an exam from any private space, such as your home or office. You use your own computer for the exam, and converse with a proctor who remotely monitors your exam via both a screen-sharing application and your webcam. Most exam appointments are available 24 hours a day, seven days a week.',
+                    ),
+                    SizedBox(height: height * 0.01),
+                    Text(
+                      'Online proctoring is available for all AWS Certification exams from our test delivery providers Pearson VUE.',
+                    ),
+                    SizedBox(height: height * 0.01),
+                    Text(
+                      'Proctoring languages and availability:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(height: height * 0.01),
+                    Text('• English: 24/7'),
+                    Text(
+                        '• Japanese: Monday – Saturday, 9 a.m. – 4 p.m. Local Time/JST'),
+                    Text(
+                        '• Spanish (Latin America): Monday – Friday, 10:00 a.m. – 5:45 p.m. EST'),
+                    Text(
+                        '• Mandarin (for customers in Mainland China): Monday – Friday, 8:00 a.m. – 5:00 p.m. Local Time/CST'),
+                    SizedBox(height: height * 0.01),
+                    GestureDetector(
+                      onTap: () {
+                        // Buka link jadwal ujian di sini
+                      },
+                      child: Text(
+                        'Schedule an AWS Certification Exam (amazon.com)',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: height * 0.02),
+              Align(
+                alignment: Alignment.bottomRight,
                 child: ElevatedButton(
+                  onPressed: () {
+                    // Tambahkan aksi saat tombol "PROGRES" ditekan
+                  },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.orangeAccent, // Button color
+                    backgroundColor: Colors.amber,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+                      borderRadius: BorderRadius.circular(12.0),
                     ),
                   ),
-                  onPressed: () {
-                    // Action for scheduling an exam
-                  },
                   child: Text(
-                    'PROGRESS',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    'PROGRES',
+                    style: TextStyle(color: Colors.black),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20), // Space below the button
-          ],
+              SizedBox(height: height * 0.02),
+            ],
+          ),
         ),
       ),
     );
--- lib/dosen/riwayat.dart
+++ lib/dosen/riwayat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../header.dart'; // Import file header.dart
+import 'descRiwayat.dart'; // Import file descRiwayat.dart
 
 class HistoryPage extends StatelessWidget {
   @override
@@ -35,6 +36,7 @@ class HistoryPage extends StatelessWidget {
               ),
               SizedBox(height: height * 0.03),
               _buildCertificationItem(
+                context: context, // Tambahkan context
                 title: 'AWS Certified Solutions Architect',
                 subtitle: 'Cloud Computing',
                 status: 'PROSES',
@@ -42,9 +44,10 @@ class HistoryPage extends StatelessWidget {
               ),
               SizedBox(height: height * 0.02),
               _buildCertificationItem(
+                context: context, // Tambahkan context
                 title: 'AWS Certified Solutions Architect',
                 subtitle: 'Cloud Computing',
-                status: 'SELESAI',
+                status: 'PROSES',
                 width: width * 0.9,
               ),
               SizedBox(height: height * 0.02),
@@ -57,52 +60,62 @@ class HistoryPage extends StatelessWidget {
   }
 
   Widget _buildCertificationItem({
+    required BuildContext context, // Tambahkan BuildContext
     required String title,
     required String subtitle,
     required String status,
     required double width,
   }) {
-    return Container(
-      width: width,
-      padding: const EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: Color(0xFF0D47A1),
-          width: 5,
+    return InkWell(
+      onTap: () {
+        // Navigasi ke halaman descRiwayat.dart
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DescRiwayat()),
+        );
+      },
+      child: Container(
+        width: width,
+        padding: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Color(0xFF0D47A1),
+            width: 5,
+          ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          Text(
-            subtitle,
-            style: TextStyle(
-              color: Colors.black54,
-              fontSize: 14,
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Text(
-              status,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
               style: TextStyle(
-                color: status == 'SELESAI' ? Colors.green : Colors.orange,
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
               ),
             ),
-          ),
-        ],
+            Text(
+              subtitle,
+              style: TextStyle(
+                color: Colors.black54,
+                fontSize: 14,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Text(
+                status,
+                style: TextStyle(
+                  color: status == 'SELESAI' ? Colors.green : Colors.orange,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
